1bc70ea62120d1e9f7b6b2c73f37fed16e0e162a
abrarimamsatria09 <092004abrar@gmail.com>
2023-05-17 17:34:55 +0700

    digunakan untuk membuat objek mahasiswa dengan nama yang diinisialisasi sesuai dengan nilai yang diberikan

--- Konstruktor/Konstruktor.cpp
+++ Konstruktor/Konstruktor.cpp
@@ -22,3 +22,13 @@ mahasiswa::mahasiswa() {
 mahasiswa :: mahasiswa(int iNim){
 	nim = iNim;
 }
+
+mahasiswa::mahasiswa(string iNama) {
+	nama + iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim=iNim;
+	nama=iNama;
+}
+
